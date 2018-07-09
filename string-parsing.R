62583f1d4bca4e045993ee2b85b181755ff806fe
JesseWB <JesseWB.C@gmail.com>
2018-07-08 22:06:58 -0700

    Fixed strsplit.citiesBySpace to work even if the city name contains a common prefix. It was breaking at dalLAS before

--- string-parsing.R
+++ string-parsing.R
@@ -1,5 +1,7 @@
-#Split strings by uppercase. Will separate acronyms
-strsplit.uppercase <- function(toSplit, acronyms=FALSE) {
+library(magrittr)
+
+#Add spaces between the uppercase letters in a string
+addSpace.uppercase <- function(toSplit, acronyms=FALSE) {
   if (acronyms==TRUE) {
     gsub("(?<=[a-z])(?=[A-Z])", " \\1", toSplit, perl=TRUE)
   }
@@ -7,3 +9,22 @@ strsplit.uppercase <- function(toSplit, acronyms=FALSE) {
     gsub("(?!^)([A-Z])", " \\1", toSplit, perl=TRUE)
   }
 }
+
+#Will separate strings by spaces, but ignore common prefixes on cities
+strsplit.citiesBySpace <- function(toSplit, ignore_case=TRUE) {
+  commonPrefix <- c("San", "Las", "Los", "New", "Ft", "Fort", "St", "Saint", "Grand", "Great", "Greater",
+                    "North", "South", "East", "West")
+
+  #Prepare the common prefixes for regex
+  expression <- commonPrefix %>% 
+    paste0("(?<!\\w)", ., collapse="|") %>%
+    paste0("(?<!", ., ")\\s")
+  
+  #Add ignore case modifier
+  if (ignore_case==TRUE) {
+    expression <- paste0("(?i)",expression)
+  }
+  strsplit(toSplit, expression, perl=TRUE)
+}
+
+#will grab the first word of a string unless it is preceded by a common city prefix
